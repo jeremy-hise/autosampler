9f0ea13b494494765a352aeb2db55d8c13312b7b
Jeremy Hise <jhise@new-host.home>
2016-06-22 16:41:12 -0400

    Change up the way the current_time is specified in the config file.

--- autosampler/autosampler.ino
+++ autosampler/autosampler.ino
@@ -34,50 +34,61 @@ char pump2Time[20];
 char pump3Time[20];
 char pump4Time[20];
 char pump5Time[20];
+char currentTime[20];
+
+int p1, p2, p3, p4, p5;
+
 
 void setup() {
   Serial.begin(9600);
   RTC.configure(3,4,5,6);
+  p1 = p2 = p3 = p4 = p5 = 0;
   initializeFromFile();
 }
 
 void loop() {
-  time_t ts_now;
+  time_t ts_now, ts_test;
+
   
   Serial.println(RTC.readDateTime());
 
   // Generate a local now timestamp
-  
   setTime(RTC.time_h(), RTC.time_m(), RTC.time_s(), RTC.date_d(), RTC.date_m(), RTC.date_y());
   
   // Create a local timestamp for pump 1
   ts_now = now();
-  Serial.print(" ts_now = ");
-  Serial.println(ts_now);
   
     // Compare
     // If time passed and pump1 flag not set
+    if(ts_now > getTimeStamp(pump1Time) && p1 == 0) {
       // Run Pump
+      p1 = 1;
+      runPump(1);
+    }
       
-  // Create a local timestamp for pump 2
-    // Compare
-    // If time passed and pump2 flag not set
+    if(ts_now > getTimeStamp(pump2Time) && p2 == 0) {
       // Run Pump
+      p2 = 1;
+      runPump(2);
+    }
 
-  // Create a local timestamp for pump 3
-    // Compare
-    // If time passed and pump3 flag not set
+    if(ts_now > getTimeStamp(pump3Time) && p3 == 0) {
       // Run Pump
+      p3 = 1;
+      runPump(3);
+    }
 
-  // Create a local timestamp for pump 4
-    // Compare
-    // If time passed and pump4 flag not set
+    if(ts_now > getTimeStamp(pump4Time) && p4 == 0) {
       // Run Pump
+      p4 = 1;
+      runPump(4);
+    }
 
-  // Create a local timestamp for pump 5
-    // Compare
-    // If time passed and pump5 flag not set
+    if(ts_now > getTimeStamp(pump5Time) && p5 == 0) {
       // Run Pump
+      p5 = 1;
+      runPump(5);
+    }
   
 
 
@@ -86,20 +97,73 @@ void loop() {
 
 }
 
+void runPump(int pump_id)
+{
+  Serial.print("Running pump ");
+  Serial.println(pump_id);
+}
+
+void handleDateTimeString(char time_string[20], int &t_hour, int &t_min, int &t_sec, int &t_day, int &t_month, int &t_year)
+{
+
+  char t_part[2];
+  
+  Serial.print("Handling ");
+  Serial.println(time_string);
+
+  memcpy(t_part, &time_string[0], 2);
+  t_hour = strtol(t_part, NULL, 10);
+
+  memcpy(t_part, &time_string[3], 2);
+  t_min = strtol(t_part, NULL, 10);
+  
+  memcpy(t_part, &time_string[6], 2);
+  t_sec = strtol(t_part, NULL, 10);
+
+  memcpy(t_part, &time_string[9], 2);
+  t_day = strtol(t_part, NULL, 10);
+
+  memcpy(t_part, &time_string[12], 2);
+  t_month = strtol(t_part, NULL, 10);
+
+  memcpy(t_part, &time_string[15], 4);
+  t_year = strtol(t_part, NULL, 10);
+
+
+}
+
 long getTimeStamp(char time_string[20]) 
 {
   char t_part[2];
-  long t_value;
+  long t_hour, t_min, t_sec, t_month, t_day, t_year;
+  time_t ts;
 
   memcpy(t_part, &time_string[0], 2);
-  t_value = strtol(t_part, NULL, 10);
+  t_hour = strtol(t_part, NULL, 10);
+
+  memcpy(t_part, &time_string[3], 2);
+  t_min = strtol(t_part, NULL, 10);
   
-  return(t_value);
+  memcpy(t_part, &time_string[6], 2);
+  t_sec = strtol(t_part, NULL, 10);
+
+  memcpy(t_part, &time_string[9], 2);
+  t_day = strtol(t_part, NULL, 10);
+
+  memcpy(t_part, &time_string[12], 2);
+  t_month = strtol(t_part, NULL, 10);
+
+  memcpy(t_part, &time_string[15], 4);
+  t_year = strtol(t_part, NULL, 10);
+
+  setTime(t_hour, t_min, t_sec, t_day, t_month, t_year);
+  ts = now();
+  return(ts);
 }
 
 int initializeFromFile()
 {
-  const size_t bufferLen = 35;
+  const size_t bufferLen = 40;
   char buffer[bufferLen];
   char log[bufferLen];
   int c_month,c_day,c_year,c_hour,c_min,c_sec, c_set;
@@ -114,8 +178,6 @@ int initializeFromFile()
   if (!digitalRead(cardDetect))
   {
     Serial.println(F("No card detected. Waiting for card."));
-    while (!digitalRead(cardDetect));
-    delay(250); // 'Debounce insertion'
     return(0);
   }
 
@@ -129,124 +191,83 @@ int initializeFromFile()
   
   if (ini.getValue("datetime", "c_set", buffer, bufferLen)) {
     c_set = strtol(buffer, NULL, 10);
+    c_set = 1;
+  }
+  
+  if(ini.getValue("datetime", "current_time", buffer, bufferLen)) {
+    strcpy(currentTime, buffer);
+    Serial.print("Current time: ");
+    Serial.println(currentTime);
   }
   
   if (ini.getValue("pumps", "pump1_time", buffer, bufferLen)) {
     strcpy(pump1Time, buffer);
-    Serial.println("Pump 1 Time"); Serial.println(pump1Time);
-   } else {
-    Serial.println("No pump1 info");
+    Serial.print("P1 "); Serial.println(pump1Time);
    }
- /*
+
    if (ini.getValue("pumps", "pump2_time", buffer, bufferLen)) {
     strcpy(pump2Time, buffer);
-    Serial.print("Pump 2 Time"); Serial.println(pump2Time);
+    Serial.print("P2 "); Serial.println(pump2Time);
    }
+   
    if (ini.getValue("pumps", "pump3_time", buffer, bufferLen)) {
     strcpy(pump3Time, buffer);
-    Serial.print("Pump 3 Time"); Serial.println(pump3Time);
+    Serial.print("P3 "); Serial.println(pump3Time);
    }
+   
    if (ini.getValue("pumps", "pump4_time", buffer, bufferLen)) {
     strcpy(pump4Time, buffer);
-    Serial.print("Pump 4 Time"); Serial.println(pump4Time);
+    Serial.print("P4 "); Serial.println(pump4Time);
    }
+   
    if (ini.getValue("pumps", "pump5_time", buffer, bufferLen)) {
     strcpy(pump5Time, buffer);
-    Serial.print("Pump 5 Time"); Serial.println(pump5Time);
+    Serial.print("P5 "); Serial.println(pump5Time);
    }
-  */
-
  
-
- 
-    if (ini.getValue("datetime", "c_month", buffer, bufferLen)) {
-      c_month = strtol(buffer, NULL, 10);
-    }
-  
-    if (ini.getValue("datetime", "c_day", buffer, bufferLen)) {
-      c_day = strtol(buffer, NULL, 10);
-    }
-    
-    if (ini.getValue("datetime", "c_year", buffer, bufferLen)) {
-      c_year = strtol(buffer, NULL, 10);
-    }
-    
-    if (ini.getValue("datetime", "c_hour", buffer, bufferLen)) {
-      c_hour = strtol(buffer, NULL, 10);
-    }
-  
-    if (ini.getValue("datetime", "c_minute", buffer, bufferLen)) {
-      c_min = strtol(buffer, NULL, 10);
-    }
-    
-    if (ini.getValue("datetime", "c_second", buffer, bufferLen)) {
-      c_sec = strtol(buffer, NULL, 10);
-    }
+  ini.close();
 
  if(c_set == 0) {
     Serial.println("Don't set datetime");
   } else {
+    handleDateTimeString(currentTime, c_hour, c_min, c_sec, c_day, c_month, c_year);
     sprintf(log, "Setting datetime: %d/%d/%d %d:%d:%d", c_month, c_day, c_year, c_hour, c_min, c_sec);
     Serial.println(log);
     
     // Day month year hour minute second
     RTC.setDateTime(c_day, c_month, c_year, c_hour, c_min, c_sec);
-    
   }
-  Serial.print("Initial datetime");
+
   Serial.println(RTC.readDateTime());
 
-  ini.close();
-
   
-
   // Rewrite the file, putting c_set to 0
-  Serial.print(fileName);
-  Serial.print(" ");
-  if (SD.exists(fileName))
-  {
-    Serial.println(F(" exists."));
-  }
-  else
-  {
-    Serial.println(F(" doesn't exist. Creating."));
-  }
-
-/*
-  SD.remove(fileName);
-
-  settingsFile = SD.open(fileName, FILE_WRITE);
-  if(settingsFile) {
-    settingsFile.println("[datetime]");
-    sprintf(log, "c_set = %d", 0);
-    settingsFile.println(log);
-    sprintf(log, "c_month = %d", c_month);
-    settingsFile.println(log);
-    sprintf(log, "c_day = %d", c_day);
-    settingsFile.println(log);
-    sprintf(log, "c_year = %d", c_year);
-    settingsFile.println(log);
-    sprintf(log, "c_hour = %d", c_hour);
-    settingsFile.println(log);
-    sprintf(log, "c_minute = %d", c_min);
-    settingsFile.println(log);
-    sprintf(log, "c_second = %d", c_sec);
-    settingsFile.println(log);
+  if(c_set == 1) {
     
-    settingsFile.println("[pumps]");
-    sprintf(log, "pump1_time = %S", pump1Time);
-    sprintf(log, "pump2_time = %S", pump2Time);
-    sprintf(log, "pump3_time = %S", pump3Time);
-    sprintf(log, "pump4_time = %S", pump4Time);
-    sprintf(log, "pump5_time = %S", pump5Time);
-    
-
-    settingsFile.close();
-  } else {
-    Serial.println("Could not open settings file for writing.");
-    return(0);
+    SD.remove(fileName);
+  
+    settingsFile = SD.open(fileName, FILE_WRITE);
+    if(settingsFile) {
+      settingsFile.println("[datetime]");
+      sprintf(log, "c_set = %d", 0);
+      settingsFile.println(log);
+      sprintf(log, "current_time = %s", currentTime);
+      
+      settingsFile.println("[pumps]");
+      sprintf(log, "pump1_time = %s", pump1Time);
+      settingsFile.println(log);
+      sprintf(log, "pump2_time = %s", pump2Time);
+      settingsFile.println(log);
+      sprintf(log, "pump3_time = %s", pump3Time);
+      settingsFile.println(log);
+      sprintf(log, "pump4_time = %s", pump4Time);
+      settingsFile.println(log);
+      sprintf(log, "pump5_time = %s", pump5Time);
+      settingsFile.println(log);    
+  
+      settingsFile.close();
+    } 
   }
-  */
 
   return(1);
 }
